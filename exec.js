f3a59be66be84e9fc4fea4959060729e064cb321
soonxf <43446813+soonxf@users.noreply.github.com>
2022-11-27 21:00:19 +0800

    无

--- exec.js
+++ exec.js
@@ -1,13 +1,14 @@
 const { exec } = require('child_process');
+const logRule = require(__dirname + '/logRule.js');
 
 const drop = (ip, name = '', siteTemp = '') => {
+  if (logRule.firewalls.includes(ip)) return;
   ip
     ? exec(
         `firewall-cmd --permanent --add-rich-rule='rule family=ipv4 source address="${ip}" drop'`,
         (err, stdout, stderr) => {
-          stdout
-            ? console.log(`加入防火墙:${stdout.replace(/\n/, '')} ${name} ${ip} ${siteTemp}`)
-            : console.log(stderr.replace(/\n/, ''));
+          stdout && console.log(`加入防火墙:${stdout.replace(/\n/, '')} ${name} ${ip} ${siteTemp}`);
+          stderr && console.log(stderr.replace(/\n/, ''));
           err && console.log('drop 错误');
         }
       )
@@ -17,10 +18,11 @@ const drop = (ip, name = '', siteTemp = '') => {
 const firewallReload = () => {
   setTimeout(function () {
     exec(`firewall-cmd --reload`, (err, stdout, stderr) => {
-      stdout ? console.log(`防火墙 reload 成功:${stdout.replace(/\n/, '')}`) : console.log(stderr.replace(/\n/, ''));
+      stdout && console.log(`防火墙 reload 成功:${stdout.replace(/\n/, '')}`);
+      stderr && console.log(stderr.replace(/\n/, ''));
       err && console.log('firewallReload 错误');
     });
-  }, 3000);
+  }, 1500);
 };
 
 module.exports.drop = drop;
